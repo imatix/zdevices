f208e64ebda3bff36a9e4cd6320a449f75b86cf1
Pieter Hintjens <ph@imatix.com>
2010-09-05 10:12:20 +0200

    License changed to LGPLv3+

--- c/generic/zdevice.c
+++ c/generic/zdevice.c
@@ -35,20 +35,18 @@
 
     Copyright (c) 1991-2010 iMatix Corporation and contributors
 
-    This file is part of the ZeroMQ Function Library: http://zfl.zeromq.org
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or (at
+    your option) any later version.
 
-    This is free software; you can redistribute it and/or modify it under
-    the terms of the Lesser GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
+    This program is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
 
-    This software is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    Lesser GNU General Public License for more details.
-
-    You should have received a copy of the Lesser GNU General Public License
-    along with this program. If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
     =========================================================================
 */
 
